15ae3f5c8a0fb6e49e1f10eeccf52c52a8b1f10d
nikola-golijanin <golijanin.nikola@outlook.com>
2023-08-18 21:17:40 +0200

    Remove println of random number

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
+    let secret_number = rand::thread_rng().gen_range(1..=10);
 
     loop {
         println!("Please input your guess.");
